eebcf27d6235d0852415e08d07c763127aeb6bd1
Bruno <bruno_151997@hotmail.com>
2021-04-28 19:52:16 -0300

    configure nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "0cf624e68668a1",
+    pass: "bed8d7b5b0dc24",
+  },
+  default: {
+    from: "Equipe GoBarber <noreply@gobarber.com>",
+  },
+};
