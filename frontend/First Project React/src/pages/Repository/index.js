2bf79284e7ffd83c1880f5c5d3940caa1a4591b8
Bruno <bruno_151997@hotmail.com>
2021-05-06 21:00:39 -0300

    loading data from api

--- frontend/First Project React/src/pages/Repository/index.js
+++ frontend/First Project React/src/pages/Repository/index.js
@@ -1,9 +1,32 @@
-import React from "react";
-
+import React, { Component } from "react";
+import api from "../../services/api";
 // import { Container } from './styles';
 
-function Repository({ match }) {
-    return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
-}
+export default class Repository extends Component {
+    async componentDidMount() {
+        const { match } = this.props;
 
-export default Repository;
+        const repoName = decodeURIComponent(match.params.repository);
+
+        const [repository, issues] = await Promise.all([
+            api.get(`/repos/${repoName}`),
+            api.get(`/repos/${repoName}/issues`),
+            {
+                params: {
+                    state: "open",
+                    per_page: 5,
+                },
+            },
+        ]);
+
+        this.setState({
+            repository: repository.data,
+            issues: issues.data,
+            loading: false,
+        });
+    }
+    render() {
+        const { repository, issues, loading } = this.state;
+        <h1>Repository</h1>;
+    }
+}
